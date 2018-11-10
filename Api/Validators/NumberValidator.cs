d82f484afa20b05779e4ec530604e5794047494a
Tobias Genberg <lorceroth@gmail.com>
2018-11-10 20:45:25 +0100

    Added simple number validator

---
+++ Api/Validators/NumberValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Api.Validators
+{
+    public class NumberValidator
+    {
+        public static string ErrorMessage = "Invalid number format.";
+
+        private string _format = @"^\+46[0-9]{9}$";
+
+        private Regex _regex;
+
+        public NumberValidator() => _regex = new Regex(_format);
+
+        public bool Validate(string number)
+        {
+            return _regex.IsMatch(number);
+        }
+    }
+}
---
+++ Tests/Validators/NumberValidatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Api.Validators;
+
+namespace Tests.Validators
+{
+    [TestClass]
+    public class NumberValidatorTest
+    {
+        [TestMethod]
+        public void Validate_ValidNumber_ReturnTrue()
+        {
+            // Arrange
+            var validator = new NumberValidator();
+            var number = "+46712312123";
+
+            // Act
+            bool validationResult = validator.Validate(number);
+
+            // Assert
+            validationResult.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Validate_TooManyDigits_ReturnFalse()
+        {
+            // Arrange
+            var validator = new NumberValidator();
+            var number = "00467562341424";
+
+            // Act
+            bool validationResult = validator.Validate(number);
+
+            // Assert
+            validationResult.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Validate_InvalidCharacters_ReturnFalse()
+        {
+            // Arrange
+            var validator = new NumberValidator();
+            var number = "T00467562341424";
+
+            // Act
+            bool validationResult = validator.Validate(number);
+
+            // Assert
+            validationResult.Should().BeFalse();
+        }
+    }
+}
