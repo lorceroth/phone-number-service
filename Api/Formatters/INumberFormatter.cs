f013876346542eb14ae21d189824b42cf74523d3
Tobias Genberg <lorceroth@gmail.com>
2018-11-11 16:21:59 +0100

    Added documentation

--- Api/Formatters/INumberFormatter.cs
+++ Api/Formatters/INumberFormatter.cs
@@ -1,7 +1,18 @@
 ﻿namespace Api.Formatters
 {
+    /// <summary>
+    /// Formats phone numbers according to Swedish standards.
+    /// </summary>
     public interface INumberFormatter
     {
+        /// <summary>
+        /// Formats a phone number.
+        /// </summary>
+        /// <param name="number">The number to format.</param>
+        /// <returns></returns>
+        /// <exception cref="System.FormatException">
+        /// Thrown when the number is invalid according to the <see cref="Api.Validators.INumberValidator">INumberValidator</see>.
+        /// </exception>
         string Format(string number);
     }
 }
--- Api/Validators/INumberValidator.cs
+++ Api/Validators/INumberValidator.cs
@@ -1,9 +1,20 @@
 ﻿namespace Api.Validators
 {
+    /// <summary>
+    /// Validates phone numbers according to Swedish standards.
+    /// </summary>
     public interface INumberValidator
     {
+        /// <summary>
+        /// The default error message.
+        /// </summary>
         string ErrorMessage { get; }
 
+        /// <summary>
+        /// Validates a phone number.
+        /// </summary>
+        /// <param name="number">The number to validate.</param>
+        /// <returns></returns>
         bool Validate(string number);
     }
 }
